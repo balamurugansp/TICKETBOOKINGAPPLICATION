2644ab402325ca5b5e451a1b72f3be1a8d4748d5
balamurugansp <spbalamurugan@gmail.com>
2023-07-28 11:37:31 +0530

    First Commit

---
+++ TICKETBOOKINGAPPLICATION/src/main/java/com/onlinemovie/ticketbooking/TicketbookingapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.onlinemovie.ticketbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketbookingapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketbookingapplicationApplication.class, args);
+	}
+
+}
---
+++ TICKETBOOKINGAPPLICATION/src/test/java/com/onlinemovie/ticketbooking/TicketbookingapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinemovie.ticketbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketbookingapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
